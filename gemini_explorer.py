6cf5582d4feaab3a70a76e788420cbff21fc8772
Leejun <estherleejunkim@gmail.com>
2024-06-28 09:42:20 +0900

    fixed bug

--- gemini_explorer.py
+++ gemini_explorer.py
@@ -3,36 +3,34 @@ import streamlit as st
 from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerativeModel, Part, Content, ChatSession
 
-project="gemini-explorer"
+# Initialize Vertex AI with project and location
+vertexai.init(project="gemini-explorer-427507", location="us-central1")
 
-vertexai.init(project=project)
-
-config=generative_models.GenerationConfig(
+config = generative_models.GenerationConfig(
     temperature=0.4
 )
 
-# Creating the model w/ config
+# Creating the model with config
 model = GenerativeModel(
-    "gemini-pro",
+    model_name="gemini-1.5-pro",
     generation_config=config
 )
 
 # Creating the chat session 
 chat = model.start_chat()
 
-
-#Now working with the Streamlit app
+# Now working with the Streamlit app
 # Helper function to display & store chat messages
 def llm_helper(chat: ChatSession, query):
-    response = chat.send_message(query) #sending query to chat model
-    output = response.candidates[0].content.parts[0].text #getting the response from the model
+    response = chat.send_message(query)  # sending query to chat model
+    output = response.candidates[0].content.parts[0].text  # getting the response from the model
     
     # Displaying the response
     with st.chat_message("model"):
         st.markdown(output)
         
     st.session_state.messages.append({
-        "role":"user",
+        "role": "user",
         "content": query
     })
     
@@ -40,36 +38,43 @@ def llm_helper(chat: ChatSession, query):
         "role": "model",
         "content": output
     })
-    
+
 # Setting the title of the Streamlit app
 st.title("Gemini Explorer")
 
 # Initializing chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    
+
 # Displaying & loading chat history if there are messages
 for index, message in enumerate(st.session_state.messages):
     content = Content(
-        role = message["role"],
-        parts = [Part(text=message["content"])] #Creates a list with a single Part object containing the message text
+        role=message["role"],
+        parts=[Part.from_text(message["content"])]  # Creates a list with a single Part object containing the message text
     )
-    
+
     # Displaying the message
     if index != 0:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-    
+
     chat.history.append(content)
-    
+
 # Capturing user input
-query = st.chat_input("Gemini Explorer") #chat_input displayes a chat input widget
+query = st.chat_input("Gemini Explorer")  # chat_input displays a chat input widget
 
 if query:
     with st.chat_message("user"):
         st.markdown(query)
     llm_helper(chat, query)
-    
+
 if len(st.session_state.messages) == 0:
-    initial_prompt = "Introduce yourself as ReX, an assistant powered by Google Gemini. You use emojis to be interactive"
+    initial_prompt = "Introduce yourself as ReX, an assistant powered by Google Gemini. You use emojis to be interactive."
     llm_helper(chat, initial_prompt)
+
+
+#resetting button
+if st.button("Reset"):
+    st.session_state.messages = [] #initializing chat history
+    chat = model.start_chat() #starting a new chat session
+    
